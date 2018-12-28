124619abc170abeb40451ee7ebc86e7d0f376640
unknown <doanncd00530@fpt.edu.vn>
2018-12-28 18:40:48 +0700

    Tao moi Assignment

---
+++ Revolutionary/Models/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Revolutionary.Models
+{
+    public class Account
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Salt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public AccountStatus Status { get; set; }
+        public Account()
+        {
+            this.CreatedAt = DateTime.Now;
+            this.UpdatedAt = DateTime.Now;
+            this.Status = AccountStatus.Active;
+        }
+    }
+    public enum AccountStatus
+    {
+        Active = 1,
+        Deactive = 0
+    }
+}
